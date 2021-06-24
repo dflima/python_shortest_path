52775ca3bf040fd854d899a02aa7c9ecdf73c2d2
Danilo <danilo.lima@sumup.com>
2021-06-24 16:42:10 -0300

    added unit tests for the endpoints

--- path/tests.py
+++ path/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class PathIndexViewTests(TestCase):
+    def test_not_implemented_yet(self):
+        response = self.client.get(reverse('path:index'))
+        self.assertEqual(response.status_code, 501)
+
+class PathConnectNodeViewTests(TestCase):
+    def test_not_implemented_yet(self):
+        response = self.client.get(reverse('path:connect_node'))
+        self.assertEqual(response.status_code, 501)

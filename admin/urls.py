4a8632f493bd071be80241a09e501249de809308
Danilo <danilo.lima@sumup.com>
2021-06-24 16:44:36 -0300

    fixed urls

--- admin/urls.py
+++ admin/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('path/', include('path.urls')),
+    path('', include('path.urls')),
     path('admin/', admin.site.urls),
 ]
--- path/urls.py
+++ path/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 app_name = 'path'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('path/', views.index, name='index'),
     path('connectNode/', views.connect_node, name='connect_node'),
 ]

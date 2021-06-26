4adb8be4dad363fb6c564c6fbf614ea9ace7a5d9
Danilo <danilo.lima@sumup.com>
2021-06-26 00:28:28 -0300

    fixed unit tests

--- path/tests.py
+++ path/tests.py
@@ -2,12 +2,17 @@ from django.test import TestCase
 from django.urls import reverse
 
 # Create your tests here.
+
 class PathIndexViewTests(TestCase):
+
     def test_not_implemented_yet(self):
         response = self.client.get(reverse('path:index'))
         self.assertEqual(response.status_code, 501)
 
+
 class PathConnectNodeViewTests(TestCase):
+
     def test_not_implemented_yet(self):
-        response = self.client.get(reverse('path:connect_node'))
+        response = self.client.post(reverse('path:connect_node'),
+                                    {'from': 'A', 'to': 'B'})
         self.assertEqual(response.status_code, 501)

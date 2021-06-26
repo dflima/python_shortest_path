4de73cbf42410bbba7ea88c5259c455a32fb0d25
Danilo <danilo.lima@sumup.com>
2021-06-26 01:59:58 -0300

    added validations

--- path/tests.py
+++ path/tests.py
@@ -1,13 +1,25 @@
 from django.test import TestCase
 from django.urls import reverse
 
+
 # Create your tests here.
 
 class PathIndexViewTests(TestCase):
 
     def test_not_implemented_yet(self):
-        response = self.client.get(reverse('path:index'))
+        response = self.client.get(reverse('path:index'), {'from': 'A',
+                                   'to': 'B'})
         self.assertEqual(response.status_code, 501)
+
+    def test_bad_request_without_parameters(self):
+        response = self.client.get(reverse('path:index'))
+        self.assertEqual(response.status_code, 400)
+
+    def test_bad_request_with_wrong_parameters(self):
+        response = self.client.get(reverse('path:index'),
+                                   {'from': 'invalid',
+                                   'to': 'parameters'})
+        self.assertEqual(response.status_code, 400)
 
 
 class PathConnectNodeViewTests(TestCase):
--- path/views.py
+++ path/views.py
@@ -1,13 +1,42 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+import re
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
 # Create your views here.
-from rest_framework.views import APIView
+from .models import Graph
 
 class PathList(APIView):
+
     def get(self, request, format=None):
-        return HttpResponse("Not implemented yet.", status=501)
+        graph = Graph()
+        graph.add_edge(0, 1);
+        graph.add_edge(0, 3);
+        graph.add_edge(1, 2);
+        graph.add_edge(3, 4);
+        graph.add_edge(3, 7);
+        graph.add_edge(4, 5);
+        graph.add_edge(4, 6);
+        graph.add_edge(4, 7);
+        graph.add_edge(5, 6);
+        graph.add_edge(6, 7);
+
+        from_node = request.query_params.get('from')
+        to_node = request.query_params.get('to')
+
+        if from_node == None or to_node == None:
+            return Response("Bad request", status=status.HTTP_400_BAD_REQUEST)
+
+        pattern = re.compile(r'^[A-Z]{1}$')
+        if not pattern.match(from_node) or not pattern.match(to_node):
+            return Response("Bad request", status=status.HTTP_400_BAD_REQUEST)
+
+        # shortest_distance = graph.shortest_distance(ord(from_node)-65, ord(to_node)-65)
+        # return Response({"Path": shortest_distance}, status=status.HTTP_200_OK)
+        return Response('Not implemented yet.', status=status.HTTP_501_NOT_IMPLEMENTED)
+
 
 class ConnectNode(APIView):
+
     def post(self, request, format=None):
-        return HttpResponse("Not implemented yet.", status=501)
+        return Response('Not implemented yet.', status=status.HTTP_501_NOT_IMPLEMENTED)

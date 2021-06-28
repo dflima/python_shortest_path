391805923dabe443aae704fe35ce8d7a73ab7048
Danilo <danilo.lima@sumup.com>
2021-06-28 02:55:53 -0300

    added BadRequestError class

--- path/views.py
+++ path/views.py
@@ -7,11 +7,13 @@ from rest_framework import status
 
 from .models import Graph
 from .admin import BFS
+from .errors import BadRequestError
 
 
 def validate_input(from_node, to_node):
     pattern = re.compile(r'^[A-Z]{1}$')
-    return pattern.match(from_node) or pattern.match(to_node)
+    if not pattern.match(from_node) or not pattern.match(to_node):
+        raise BadRequestError
 
 class PathList(APIView):
 
@@ -21,42 +23,43 @@ class PathList(APIView):
 
         graph = g.build_graph()
 
-        from_node = request.query_params.get('from', '')
-        to_node = request.query_params.get('to', '')
+        try:
+            from_node = request.query_params.get('from', '')
+            to_node = request.query_params.get('to', '')
+            validate_input(from_node, to_node)
 
-        if not validate_input(from_node, to_node):
+            shortest_distance = bfs.shortest_distance(
+                graph=graph,
+                source=ord(from_node)-65,
+                destination=ord(to_node)-65,
+            )
+        except BadRequestError:
             return Response('Bad request',
                             status=status.HTTP_400_BAD_REQUEST)
-
-        shortest_distance = bfs.shortest_distance(
-            graph=graph,
-            source=ord(from_node)-65,
-            destination=ord(to_node)-65,
-        )
-
-        return Response({'Path': shortest_distance},
-                        status=status.HTTP_200_OK)
+        else:
+            return Response({'Path': shortest_distance},
+                            status=status.HTTP_200_OK)
 
 
 class ConnectNode(APIView):
 
     def post(self, request, format=None):
-        from_node = request.data.get('from', '')
-        to_node = request.data.get('to', '')
-
-        if not validate_input(from_node, to_node):
-            return Response('Bad request',
-                            status=status.HTTP_400_BAD_REQUEST)
-
-        node_from, created = Graph.objects.get_or_create(node=ord(from_node)-65)
-        node_from.update_vertices(to_node)
-
-        node_to, created = Graph.objects.get_or_create(node=ord(to_node)-65)
-        node_to.update_vertices(from_node)
-
         try:
+            from_node = request.data.get('from', '')
+            to_node = request.data.get('to', '')
+            validate_input(from_node, to_node)
+
+            node_from, created = Graph.objects.get_or_create(node=ord(from_node)-65)
+            node_from.update_vertices(to_node)
+
+            node_to, created = Graph.objects.get_or_create(node=ord(to_node)-65)
+            node_to.update_vertices(from_node)
+
             node_from.save()
             node_to.save()
+        except BadRequestError:
+            return Response('Bad request',
+                            status=status.HTTP_400_BAD_REQUEST)
         except:
             return Response('Internal Error', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         else:

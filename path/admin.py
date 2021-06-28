89e60cf49ac41b0ae0e9595f08aaad2d9dd3b779
Danilo <danilo.lima@sumup.com>
2021-06-28 02:32:55 -0300

    reorganized bfs functions

--- path/admin.py
+++ path/admin.py
@@ -1,3 +1,56 @@
+from collections import defaultdict
 from django.contrib import admin
 
 # Register your models here.
+class BFS:
+    def add_edge(self, graph, source, destination):
+        graph[source].append(destination)
+        graph[destination].append(source)
+
+    def BFS(self, graph, source, destination, predecessor, distance):
+        vertices = len(graph)
+        queue = []
+        visited = [False for i in range(vertices)]
+
+        for i in range(vertices):
+            distance[i] = 1000000
+            predecessor[i] = -1
+
+        visited[source] = True
+        distance[source] = 0
+        queue.append(source)
+
+        while (len(queue) != 0):
+            u = queue[0];
+            queue.pop(0);
+            for i in range(len(graph[u])):
+                if (visited[graph[u][i]] == False):
+                    visited[graph[u][i]] = True
+                    distance[graph[u][i]] = distance[u] + 1;
+                    predecessor[graph[u][i]] = u
+                    queue.append(graph[u][i])
+
+                    if (graph[u][i] == destination):
+                        return True
+        return False
+
+    def shortest_distance(self, graph, source, destination):
+        predecessor = defaultdict(list)
+        distance = defaultdict(list)
+
+        if (self.BFS(graph, source, destination, predecessor, distance) == False):
+            print("Given source and destination are not connected")
+            return ""
+
+        path = []
+        crawl = destination
+        path.append(crawl)
+
+        while (predecessor[crawl] != -1):
+            path.append(predecessor[crawl])
+            crawl = predecessor[crawl]
+
+        path.reverse()
+        str_path = list(map(lambda i: chr(i+65), path))
+
+        return ",".join(str_path)
--- path/models.py
+++ path/models.py
@@ -1,65 +1,29 @@
 from collections import defaultdict
 from django.db import models
 
+
 # Create your models here.
+
 class Graph(models.Model):
+
     class Meta:
+
         db_table = 'graph'
 
     node = models.IntegerField(db_index=True)
     vertices = models.CharField(max_length=200)
 
-    def __init__(self):
-        self.graph = defaultdict(list)
+    def build_graph(self):
+        graph = defaultdict(list)
+        nodes = Graph.objects.all()
+        for node in nodes:
+            vertices_list = node.vertices.split(',')
+            vertices = list(map(lambda v: int(v), vertices_list))
+            graph[node.node] = vertices
+        return graph
 
-    def add_edge(self, source, destination):
-        self.graph[source].append(destination)
-        self.graph[destination].append(source)
-
-    def BFS(self, source, destination, predecessor, distance):
-        vertices = len(self.graph)
-        queue = []
-        visited = [False for i in range(vertices)]
-
-        for i in range(vertices):
-            distance[i] = 1000000
-            predecessor[i] = -1
-
-        visited[source] = True
-        distance[source] = 0
-        queue.append(source)
-
-        while (len(queue) != 0):
-            u = queue[0];
-            queue.pop(0);
-            for i in range(len(self.graph[u])):
-                if (visited[self.graph[u][i]] == False):
-                    visited[self.graph[u][i]] = True
-                    distance[self.graph[u][i]] = distance[u] + 1;
-                    predecessor[self.graph[u][i]] = u
-                    queue.append(self.graph[u][i])
-
-                    if (self.graph[u][i] == destination):
-                        return True
-        return False
-
-    def shortest_distance(self, s, destination):
-        predecessor = defaultdict(list)
-        distance = defaultdict(list)
-
-        if (self.BFS(s, destination, predecessor, distance) == False):
-            print("Given source and destination are not connected")
-            return ""
-
-        path = []
-        crawl = destination
-        path.append(crawl)
-
-        while (predecessor[crawl] != -1):
-            path.append(predecessor[crawl])
-            crawl = predecessor[crawl]
-
-        path.reverse()
-        str_path = [str(i) for i in path]
-
-        return ",".join(str_path)
+    def update_vertices(self, to_node):
+        if len(self.vertices) >= 1:
+            self.vertices += ",{:d}".format(ord(to_node)-65)
+        else:
+            self.vertices = "{:d}".format(ord(to_node)-65)
